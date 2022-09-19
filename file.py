089186f1875e68a363b7ea6e95cc9b4a3a0cfd0e
Elia <iperstracchino@gmail.com>
2022-09-19 22:30:08 +0200

    lib

---
+++ file.py
@@ -0,0 +1,4 @@
+import Lib.WORDS.import_docvx as Doc
+
+Doc.wordTOcsv("C:\\Users\\ipers\\Desktop\\ITS\CERRI\\AUDIT\\py\\GITWORDS\\PRODOTTO2.docx",'C:\\Users\\ipers\\Desktop\\ITS\CERRI\\AUDIT\\py\\GITWORDS\\OUT.CSV')
+Doc.csvTOword('C:\\Ricette\\',"C:\\Users\\ipers\\Desktop\\ITS\CERRI\\AUDIT\\py\\GITWORDS\\")
